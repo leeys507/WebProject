81dd650bda327d54ed848c51a678e1653051e862
이영석 <leeys507@naver.com>
2020-11-06 17:11:16 +0900

    paste

--- src/main/java/com/wp/domain/boardlike/BoardLike.java
+++ src/main/java/com/wp/domain/boardlike/BoardLike.java
@@ -7,8 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Getter
 @Setter
