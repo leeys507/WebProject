d55fd052a269469cb4794830ee3e4a18614211cc
안송강 <songgao12@naver.com>
2020-11-13 01:41:50 +0900

    master

---
+++ src/main/java/com/wp/controller/home/HomeViewController.java
@@ -0,0 +1,27 @@
+package com.wp.controller.home;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+public class HomeViewController {
+	private final HttpSession httpSession;
+	
+    @GetMapping("/yuhome/index")    // view
+    public String openHomeIndexView(Model model) {
+		model.addAttribute("userSid",httpSession.getAttribute("sid"));
+        return "home/index";
+    }
+    
+	@GetMapping(value = "/yuhome/login")
+	public String openHomeLoginView(Model model) {
+		model.addAttribute("userSid",httpSession.getAttribute("resSid"));
+		return "home/login";
+	}
+}
---
+++ src/main/java/com/wp/domain/matching/dto/MatchingUpdateDTO.java
@@ -0,0 +1,12 @@
+package com.wp.domain.matching.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MatchingUpdateDTO {
+    private String title;
+    private String content;
+    private String boardtype;
+}
